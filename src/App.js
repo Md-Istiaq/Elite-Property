87f95d06a798c584d969fcf145adb4f11c5ee416
Md-Istiaq <mdistiaq102@outlook.com>
2022-06-04 20:06:47 +0600

    Home page & banner created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './Shared/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Blogs from './Pages/Blogs/Blogs';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/blogs' element={<Blogs/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row">
+    <img src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class=" rounded-lg shadow-2xl lg:w-1/2" />
+    <div>
+      <h1 class="text-5xl font-bold">Discover the ideal property here</h1>
+      <p class="py-6">Find a variety of properties that suits you very easily. Forget all difficulties in finding a residence for you.</p>
+      <div className='lg:flex lg:flex-cols'>
+      <div className=" lg:m-10 ">
+      <h1 className='text-3xl text-green-500 text-left'>1.5k+</h1>
+      <p className='text-left text-2xl'>Properties</p>
+      </div>
+      <div className=" lg:m-10">
+      <h1 className='text-3xl text-green-500 text-left'>2k+</h1>
+      <p className='text-left text-2xl'>Happy Customer</p>
+      </div>
+      <div className=" lg:m-10">
+      <h1 className='text-3xl text-green-500 text-left'>280+</h1>
+      <p className='text-left text-2xl'>Reviews</p>
+      </div>
+      </div>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
