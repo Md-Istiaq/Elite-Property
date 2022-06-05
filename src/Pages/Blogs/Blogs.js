316897d8325d0a244476df780c043f6b6c34a411
Md-Istiaq <mdistiaq102@outlook.com>
2022-06-05 23:25:26 +0600

    blogs route  added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 const Blogs = () => {
+    const [blogs , setBlogs] = useState([])
+    useEffect(() =>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    } ,[])
     return (
         <div>
-            <h1>this is blogs</h1>
+            <h1 className='text-3xl text-green-400 m-10 '>Real Estate Questions to Expect from Home Sellers & Home Buyers</h1>
+            <div>
+                {
+                    blogs.map(blog => 
+                        <div className='border border-green-400 m-10 p-5 rounded-xl'>
+                        <h1 className='text-xl font-bold'>{blog.title}</h1>
+                        <p>{blog.blog}</p>    
+                        </div>
+                        )
+                }
+            </div>
         </div>
     );
 };
